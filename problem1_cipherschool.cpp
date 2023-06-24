e4f9e5636df0dbcdcd09048478a4e5a85ca35b3f
Satvikbhanwal <60311891+satvik5678@users.noreply.github.com>
2023-06-24 23:34:43 +0530

    Add files via upload

---
+++ problem1_cipherschool.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"hello world"<<endl;
+}
+
+#include<iostream>
+using namespace std;
+
+int sum(int a,int b)
+{
+    return a+b;
+}
+
+int main()
+{
+    int x,y;
+    cin>>x>>y;
+    sum(x,y);
+}
